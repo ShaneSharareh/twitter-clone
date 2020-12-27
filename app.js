7583a5227a3c19c761050a1c352ee172cf9b34a4
shanesharareh <shane.s.sharareh@gmail.com>
2020-12-26 23:32:18 -0800

    create frontend of twitter clone

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-app.get("/", (req, res) => res.send("Hello yo"));
+// You can now delete our 'Hello World' route
+//app.get("/", (req, res) => res.send("Hello yo"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 mongoose
   .connect(db, {
       useUnifiedTopology: true,
        useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+  app.use(bodyParser.urlencoded({ extended: false }));// needed for postman requests
+  app.use(bodyParser.json());//needed for responding to json requests
+
+app.use(passport.initialize());
+ require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
---
+++ frontend/src/index.js
@@ -0,0 +1,48 @@
+//src/index.js 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from './components/root'
+
+import configureStore from './store/store';
+
+import jwt_decode from 'jwt-decode';
+
+import {setAuthToken} from './util/session_api_util';
+
+import {logout} from './actions/session_action';
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    let store;
+
+    //token being set as a common header for all axios requests 
+    if (localStorage.jwtToken){
+
+        setAuthToken(localStorage.jwtToken);
+
+        // decode the token to obtain the users information
+        const decodedUser = jwt_decode(localStorage.jwtToken);
+
+        // Create a preconfigured state we can immediately add to our store
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    // If the user's token has expired
+    if (decodedUser.exp < currentTime) {
+      // Logout the user and redirect to the login page
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+  } else {
+    // If this is a first time user, start with an empty store
+    store = configureStore({});
+  }
+  // Render our root component and pass in the store as a prop
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<Root store={store} />, root);
+});
